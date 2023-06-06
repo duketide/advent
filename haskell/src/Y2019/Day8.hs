279f6a756341932ebba655c123e701510ae657d8
duketide <duketide11@gmail.com>
2023-06-06 18:45:53 -0500

    clean up 2019 day 8 haskell

--- haskell/src/Y2019/Day8.hs
+++ haskell/src/Y2019/Day8.hs
@@ -15,21 +15,16 @@ layer = col * row
 p1Solve :: String -> (Int, Int, Int, Int, Int, Int)
 p1Solve = foldr next (0, 0, 0, 1, layer, 0)
   where
-    next x (zeroes, ones, twos, count, cMin, win) = (nZero, nOne, nTwo, nCount, nMin, nWin)
+    next x (zeroes, ones, twos, count, cMin, win)
+      | isEnd = (0, 0, 0, count + 1, nMin, nWin)
+      | otherwise = (z, o, t, count + 1, cMin, win)
       where
-        nCount = count + 1
         isEnd = count `mod` layer == 0
-        isZero = x == '0'
-        isOne = x == '1'
-        isTwo = x == '2'
-        z = if isZero then zeroes + 1 else zeroes
-        nZero = if isEnd then 0 else z
-        o = if isOne then ones + 1 else ones
-        nOne = if isEnd then 0 else o
-        t = if isTwo then twos + 1 else twos
-        nTwo = if isEnd then 0 else t
-        nMin = if isEnd then min cMin z else cMin
-        nWin = if isEnd && z < cMin then o * t else win
+        z = if x == '0' then zeroes + 1 else zeroes
+        o = if x == '1' then ones + 1 else ones
+        t = if x == '2' then twos + 1 else twos
+        nMin = min cMin z
+        nWin = if z < cMin then o * t else win
 
 p2Map :: String -> IntMap Char
 p2Map s = snd $ foldr (\x (ind, acc) -> (ind + 1, IM.insertWith (\n o -> if n == '2' then o else n) (ind `mod` layer) x acc)) (0, IM.empty) s
