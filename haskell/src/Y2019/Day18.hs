2132a3e8d05efdaf9bb42e6fc9d0703227859fa6
duketide <duketide11@gmail.com>
2023-06-22 12:05:45 -0500

    2019 day 18 part 2 haskell

--- haskell/src/Y2019/Day18.hs
+++ haskell/src/Y2019/Day18.hs
@@ -22,6 +22,16 @@ data State = State
 
 type StateMemo = Map Point (Map (Set Char) Int)
 
+data State2 = State2
+  { one :: State,
+    two :: State,
+    three :: State,
+    four :: State
+  }
+  deriving (Eq, Ord, Show)
+
+type StateMemo2 = Map State2 Int
+
 solve :: IO (Int, Int)
 solve = do
   input <- getInput "2019" "18"
@@ -30,12 +40,12 @@ solve = do
       startState = State {doors = S.empty, curr = start, turn = 0}
       input2 = mapP2 mp start
       startStates2 =
-        [ startState {curr = (x0 - 1, y0 - 1)},
-          startState {curr = (x0 + 1, y0 - 1)},
-          startState {curr = (x0 + 1, y0 + 1)},
+        State2
+          startState {curr = (x0 - 1, y0 - 1)}
+          startState {curr = (x0 + 1, y0 - 1)}
+          startState {curr = (x0 + 1, y0 + 1)}
           startState {curr = (x0 - 1, y0 + 1)}
-        ]
-  return (p1 mp startState numKeys, 0)
+  return (p1 mp startState numKeys, p2 input2 startStates2 numKeys)
 
 mapFloor :: String -> (Map Point Char, Point)
 mapFloor = fst . foldr f ((M.empty, (-1, -1)), (0, 0))
@@ -55,12 +65,12 @@ mapP2 mp (x, y) = next
     next = foldr (`M.insert` '#') mp lst
 
 p1 :: Map Point Char -> State -> Int -> Int
-p1 mp st nk = go (H.singleton (0, st)) 0 M.empty
+p1 mp st nk = go (H.singleton (0, st)) M.empty
   where
-    go :: MinPrioHeap Int State -> Int -> StateMemo -> Int
-    go hp n sm
+    go :: MinPrioHeap Int State -> StateMemo -> Int
+    go hp sm
       | d >= 2 * nk = pr
-      | otherwise = go hp1 (n + 1) sm'
+      | otherwise = go hp1 sm'
       where
         ((pr, pt), hp0) = fromMaybe (error "empty heap") $ H.view hp
         d = length $ doors pt
@@ -71,6 +81,49 @@ p1 mp st nk = go (H.singleton (0, st)) 0 M.empty
               where
                 next = M.insert (doors st) (turn st) $ fromMaybe M.empty $ M.lookup (curr st) smem
 
+p2 :: Map Point Char -> State2 -> Int -> Int
+p2 mp st nk = go (H.singleton (0, st)) 0 M.empty
+  where
+    go :: MinPrioHeap Int State2 -> Int -> StateMemo2 -> Int
+    go hp n sm
+      | d >= 2 * nk = pr
+      | otherwise = go hp1 (n + 1) sm'
+      where
+        ((pr, pt), hp0) = fromMaybe (error "empty heap") $ H.view hp
+        d = length $ doors $ one pt
+        nextOnes = p2Growth pt 1 mp
+        nextTwos = p2Growth pt 2 mp
+        nextThrees = p2Growth pt 3 mp
+        nextFours = p2Growth pt 4 mp
+        nextS' = foldr S.union nextOnes [nextTwos, nextThrees, nextFours]
+        nextS = S.filter (\x -> turn (one x) < fromMaybe (10 ^ 9) (M.lookup (zeroOut x) sm)) nextS'
+        (hp1, sm') = foldr (\x (h, m) -> (H.insert (turn $ one x, x) h, f x m)) (hp0, sm) nextS
+          where
+            f st = M.insert (zeroOut st) (turn $ one st)
+
+p2Growth :: State2 -> Int -> Map Point Char -> Set State2
+p2Growth s2 n mp = nextS
+  where
+    (State2 (State _ c1 _) (State _ c2 _) (State _ c3 _) (State _ c4 _)) = s2
+    chng = case n of
+      1 -> one s2
+      2 -> two s2
+      3 -> three s2
+      4 -> four s2
+      _ -> error "problem in p2Growth"
+    changedStates = bfsGrowth mp chng
+    nextS = foldr g S.empty changedStates
+      where
+        g st = S.insert (f st)
+        f st = case n of
+          1 -> s2 {one = st, two = st {curr = c2}, three = st {curr = c3}, four = st {curr = c4}}
+          2 -> s2 {one = st {curr = c1}, two = st, three = st {curr = c3}, four = st {curr = c4}}
+          3 -> s2 {one = st {curr = c1}, two = st {curr = c2}, three = st, four = st {curr = c4}}
+          4 -> s2 {one = st {curr = c1}, two = st {curr = c2}, three = st {curr = c3}, four = st}
+
+zeroOut :: State2 -> State2
+zeroOut (State2 o t th fr) = State2 {one = o {turn = 0}, two = t {turn = 0}, three = th {turn = 0}, four = fr {turn = 0}}
+
 bfsGrowth :: Map Point Char -> State -> Set State
 bfsGrowth mp c@(State d (x, y) t) = go [c] S.empty S.empty 0
   where
@@ -80,7 +133,6 @@ bfsGrowth mp c@(State d (x, y) t) = go [c] S.empty S.empty 0
         nSeen = S.union seen (S.fromList . fmap curr $ locs)
         nLocs' = locs' >>= growState mp seen
         nLocs = nub nLocs'
-        -- this locs' trick (don't go through a key to get to another key) cut run time from ~7 minutes to < 10 seconds
         locs' = filter (\x -> let t = fromMaybe '?' (M.lookup (curr x) mp) in not (t `notElem` doors x && isLower t)) locs
         nSts = S.union sts $ foldr g S.empty locs
         g (State d cr t) acc = if isLower (tile cr) && not (S.member (tile cr) d) then S.insert (State (S.insert (toUpper (tile cr)) $ S.insert (tile cr) d) cr t) acc else acc
