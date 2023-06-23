39f3c5d7a6b01211b5e183ccdd3bae541b72781a
duketide <duketide11@gmail.com>
2023-06-23 08:19:33 -0500

    2019 day 20 part 2 haskell

--- haskell/src/Y2019/Day20.hs
+++ haskell/src/Y2019/Day20.hs
@@ -2,6 +2,7 @@ module Y2019.Day20 (solve) where
 
 import AOC (getInput)
 import Data.Char (isUpper)
+import Data.List (maximumBy, minimumBy)
 import Data.Map (Map)
 import qualified Data.Map as M
 import Data.Maybe (fromMaybe)
@@ -10,6 +11,8 @@ import qualified Data.Set as S
 
 type Point = (Int, Int)
 
+type RPoint = (Point, Int)
+
 solve :: IO (Int, Int)
 solve = do
   input <- getInput "2019" "20"
@@ -17,8 +20,34 @@ solve = do
       start = head $ portals M.! "AA"
       end = head $ portals M.! "ZZ"
       pMap = portalMap portals
+      xMax = fst . maximumBy (\(x, _) (x', _) -> x `compare` x') $ M.elems pMap
+      xMin = fst . minimumBy (\(x, _) (x', _) -> x `compare` x') $ M.elems pMap
+      yMax = snd . maximumBy (\(_, y) (_, y') -> y `compare` y') $ M.elems pMap
+      yMin = snd . minimumBy (\(_, y) (_, y') -> y `compare` y') $ M.elems pMap
       fullMap = rawMap input
-  return (bfs start end fullMap pMap, 0)
+  return (bfs start end fullMap pMap, bfs' (start, 0) (end, 0) fullMap pMap (xMin, xMax, yMin, yMax))
+
+bfs' :: RPoint -> RPoint -> Map Point Char -> Map Point Point -> (Int, Int, Int, Int) -> Int
+bfs' st end mp portals bds@(xMin, xMax, yMin, yMax) = go (S.singleton st) (S.singleton st) 0
+  where
+    go :: Set RPoint -> Set RPoint -> Int -> Int
+    go q seen n
+      | S.member end q = n
+      | otherwise = go nextQ (S.union nextQ seen) (n + 1)
+      where
+        nextQ' = foldr (\x acc -> S.union acc (mover' x mp portals bds)) S.empty q
+        nextQ = S.filter (\x -> not (S.member x seen)) nextQ'
+
+mover' :: RPoint -> Map Point Char -> Map Point Point -> (Int, Int, Int, Int) -> Set RPoint
+mover' ((x, y), lvl) mp prt (xMin, xMax, yMin, yMax) = s
+  where
+    s = S.filter (\(pt@(x0, y0), lvl) -> pt /= (-1, -1) && lvl >= 0 && isJustDot (M.lookup pt mp)) raw
+    raw = S.fromList [((x, y - 1), lvl), ((x, y + 1), lvl), ((x - 1, y), lvl), ((x + 1, y), lvl), f]
+    f
+      | x == xMin || x == xMax || y == yMin || y == yMax = (p, lvl - 1)
+      | otherwise = (p, lvl + 1)
+      where
+        p = fromMaybe (-1, -1) $ M.lookup (x, y) prt
 
 bfs :: Point -> Point -> Map Point Char -> Map Point Point -> Int
 bfs st end mp portals = go (S.singleton st) (S.singleton st) 0
