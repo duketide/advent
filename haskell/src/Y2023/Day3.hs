08c6f436232044ccdc905043ece2595500a6ed33
duketide <duketide11@gmail.com>
2023-12-03 09:34:56 -0600

    2023 d3p2

--- haskell/src/Y2023/Day3.hs
+++ haskell/src/Y2023/Day3.hs
@@ -2,11 +2,17 @@ module Y2023.Day3 (solve) where
 
 import AOC (Pair, getInput, nbrs)
 import Data.Char (isDigit)
+import Data.Map (Map)
+import qualified Data.Map as M
+import Data.Maybe (fromMaybe)
 import Data.Set (Set)
 import qualified Data.Set as S
 
 isDigOrDot :: Char -> Bool
 isDigOrDot c = isDigit c || c == '.'
+
+isGear :: Char -> Bool
+isGear c = c == '*'
 
 getSymbolCoords :: [String] -> Set Pair
 getSymbolCoords = snd . foldl lineFolder ((0, -1), S.empty)
@@ -21,8 +27,29 @@ pointChecker (p@(x, y), s) ch
   where
     next = (x + 1, y)
 
+getGearCoords :: [String] -> Set Pair
+getGearCoords = snd . foldl gearFolder ((0, -1), S.empty)
+
+gearFolder :: (Pair, Set Pair) -> String -> (Pair, Set Pair)
+gearFolder ((_, y), s) = foldl gearChecker ((0, y + 1), s)
+
+gearChecker :: (Pair, Set Pair) -> Char -> (Pair, Set Pair)
+gearChecker (p@(x, y), s) ch
+  | isGear ch = (next, S.insert p s)
+  | otherwise = (next, s)
+  where
+    next = (x + 1, y)
+
 numsAndNbrs :: [String] -> [(Int, Set Pair)]
 numsAndNbrs = snd . foldl lineRecurser (0, [])
+
+tallyNbrs :: [(Int, Set Pair)] -> Map Pair [Int]
+tallyNbrs = foldr singleTally M.empty
+
+singleTally :: (Int, Set Pair) -> Map Pair [Int] -> Map Pair [Int]
+singleTally (num, s) mp = S.foldr func mp s
+  where
+    func pt acc = M.insert pt (num : fromMaybe [] (M.lookup pt acc)) acc
 
 lineRecurser :: (Int, [(Int, Set Pair)]) -> String -> (Int, [(Int, Set Pair)])
 lineRecurser (y, acc) = go ((0, y), [], S.empty, acc)
@@ -45,7 +72,18 @@ p1 inp = sum . map fst . filter f . numsAndNbrs $ inp
     f :: (Int, Set Pair) -> Bool
     f (_, s) = not $ null $ S.intersection s syms
 
+p2 :: [String] -> Int
+p2 inp = S.foldr func 0 gears
+  where
+    tally = tallyNbrs . numsAndNbrs $ inp
+    gears = getGearCoords inp
+    func p accum
+      | length l == 2 = accum + (head l * l !! 1)
+      | otherwise = accum
+      where
+        l = fromMaybe [] $ M.lookup p tally
+
 solve :: IO (Int, Int)
 solve = do
   input <- lines <$> getInput "2023" "3"
-  return (p1 input, 1)
+  return (p1 input, p2 input)
