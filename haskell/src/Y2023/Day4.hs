0cc4d73c93dadde2d1f6897e7305d4e2aefcad1f
duketide <duketide11@gmail.com>
2023-12-04 10:38:51 -0600

    2023 d4 tighten

--- haskell/src/Y2023/Day4.hs
+++ haskell/src/Y2023/Day4.hs
@@ -1,42 +1,29 @@
 module Y2023.Day4 (solve) where
 
 import AOC (getInput)
+import Data.List (intersect)
 import Data.List.Split (splitOn)
 import Data.Map (Map)
 import qualified Data.Map as M
 import Data.Maybe (fromMaybe)
-import Data.Set (Set)
-import qualified Data.Set as S
-
--- using sets assumes no duplicate numbers in winner list or "your numbers" list for a given card
-data Card = Card
-  { winners :: Set String,
-    nums :: Set String
-  }
-  deriving (Show)
 
 type P2State = (Int, Map Int Int, Int)
 
 p1 :: [String] -> Int
-p1 = foldr f 0 . map2wins
+p1 = foldr f 0 . inpParse
   where
     f wins acc = acc + if wins > 0 then 2 ^ (wins - 1) else 0
 
-map2wins :: [String] -> [Int]
-map2wins = map f . inpParse
-  where
-    f c = S.size $ S.intersection (winners c) (nums c)
-
-inpParse :: [String] -> [Card]
+inpParse :: [String] -> [Int]
 inpParse = map (f . map words . splitOn "|")
   where
-    f [_ : _ : t, ys] = Card (S.fromList t) (S.fromList ys)
+    f [_ : _ : t, ys] = length $ t `intersect` ys
     f x = error "bad parse"
 
 p2 :: [String] -> Int
 p2 inp = res
   where
-    (_, _, res) = foldl p2tally (1, M.empty, 0) (map2wins inp)
+    (_, _, res) = foldl p2tally (1, M.empty, 0) (inpParse inp)
 
 p2tally :: P2State -> Int -> P2State
 p2tally (idx, mults, result) wins = (idx + 1, newMults, result + currCount)
